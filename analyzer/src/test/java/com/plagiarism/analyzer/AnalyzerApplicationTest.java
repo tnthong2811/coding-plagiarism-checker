dd893c0cb8c3027c78b9c87e382dfd063375f69f
tnthong2811 <tnthong2811@users.noreply.github.com>
2026-09-04 17:40:43 +0700

    Fix backend test configuration

--- analyzer/src/test/java/com/plagiarism/analyzer/AnalyzerApplicationTest.java
+++ analyzer/src/test/java/com/plagiarism/analyzer/AnalyzerApplicationTest.java
@@ -2,8 +2,10 @@ package com.plagiarism.analyzer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AnalyzerApplicationTest {
 
     @Test
